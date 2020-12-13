e910b012d95ba3552ebb72fcb09289d78f9efa89
Dimitri <darkyne.gaming@outlook.de>
2020-12-14 00:36:52 +0100

    Updated version number & added dependency for eft-market-api

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // config for API Keys and Co
 const Config = require('./includes/config.js').default;
 const conf = new Config();
-// Discord
-const Discord = require('discord.js')
-const client = new Discord.Client();
 // Commands
 const Commands = require('./commands/commands').default;
 const cmd = new Commands();
+// Discord
+const Discord = require('discord.js')
+const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Bot online as ${client.user.tag}`);
     client.user.setActivity('Javascript');
-})
+});
 
 client.on('message', msg => {
     if (msg.author == client.user) { 
         return; 
     }  else if (msg.content.startsWith(cmd.key)) {
-        let splitCommand = msg.content.substr(1).split(" ")
-        let keyword = splitCommand[0].toLowerCase()
-        let args = splitCommand.slice(1)
+        let splitCommand = msg.content.substr(1).split(" ");
+        let keyword = splitCommand[0].toLowerCase();
+        let args = splitCommand.slice(1);
 
         cmd.Execute(keyword, args, msg);
     }
-})
+});
 
-client.login(conf.discordKey)
+client.login(conf.discordKey);
